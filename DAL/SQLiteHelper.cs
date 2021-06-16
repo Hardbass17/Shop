cecbf9414439647aae220815120620a59ca6b8c7
Hardbass17 <80974957+Hardbass17@users.noreply.github.com>
2021-06-16 16:52:11 +0500

    Добавьте файлы проекта.

---
+++ DAL/SQLiteHelper.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DAL
+{
+    class SQLiteHelper
+    {
+        internal static List<User> GetUsers()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(@"Data Source=Shop.sqlite;Version=3;"))
+                {
+                    connection.Open();
+
+                    using (var cmd = new SQLiteCommand("SELECT id, login, passwrd, sotrudniki.name_sotr, root FROM Users, Sotrudniki WHERE sotrudniki.id = users.id_sotr", connection))
+                    {
+                        using (var rdr = cmd.ExecuteReader())
+                        {
+                            List<User> users = new List<User>();
+
+                            while (rdr.Read())
+                            {
+                                users.Add(new User
+                                {
+                                    Id = rdr.GetInt32(0),
+                                    Login = rdr.GetString(1),
+                                    Passwrd = rdr.GetString(2),
+                                    Name_sotr = rdr.GetString(3),
+                                    Root = rdr.GetString(4),
+                                });
+                            }
+
+                            return users;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return null;
+        }
+
+//        internal static bool AddUser(User user)
+//        {
+//            try
+//            {
+//                using (var connection = new SQLiteConnection(@"Data Source=Shop.sqlite;Version=3;"))
+//                {
+//                    connection.Open();
+
+//                    using (var cmd = new SQLiteCommand($@"INSERT INTO users (
+//                      user_name,
+//                      name,
+//                      date_created
+//                  )
+//                  VALUES (
+//                      '{user.UserName}',
+//                      '{user.Name}',
+//                      '{user.Date:yyyy-MM-dd}');", connection))
+//                    {
+//                        var res = cmd.ExecuteNonQuery();
+//                        return true;
+//                    }
+//                }
+//            }
+//            catch (Exception ex) { Console.WriteLine(ex.Message); }
+//            return false;
+//        }
+
+//        internal static bool SaveUser(User user)
+//        {
+//            try
+//            {
+//                using (var connection = new SQLiteConnection(@"Data Source=Shop.sqlite;Version=3;"))
+//                {
+//                    connection.Open();
+
+//                    using (var cmd = new SQLiteCommand($@"UPDATE users  
+//SET
+//user_name = '{user.UserName}',
+//name = '{user.Name}',
+//date_created = '{user.Date:yyyy-MM-dd}'
+//WHERE id = {user.Id};", connection))
+//                    {
+//                        var res = cmd.ExecuteNonQuery();
+//                        return true;
+//                    }
+//                }
+//            }
+//            catch (Exception ex) { Console.WriteLine(ex.Message); }
+//            return false;
+//        }
+
+//        internal static bool DeleteUser(int id)
+//        {
+//            try
+//            {
+//                using (var connection = new SQLiteConnection(@"Data Source=Shop.sqlite;Version=3;"))
+//                {
+//                    connection.Open();
+
+//                    using (var cmd = new SQLiteCommand($@"DELETE FROM users WHERE id = {id};", connection))
+//                    {
+//                        var res = cmd.ExecuteNonQuery();
+//                        return true;
+//                    }
+//                }
+//            }
+//            catch (Exception ex) { Console.WriteLine(ex.Message); }
+//            return false;
+//        }
+    }
+}
---
+++ DAL/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Shop.DAL
+{
+    internal class User
+    {
+        [DisplayName("ID пользователя")]
+        public int Id { get; internal set; }
+        [DisplayName("Логин")]
+        public string Login { get; internal set; }
+        [DisplayName("Пароль")]
+        public string Passwrd { get; internal set; }
+        [DisplayName("Сотрудник")]
+        public string Name_sotr { get; internal set; }
+        [DisplayName("Права")]
+        public string Root { get; internal set; }
+    }
+}
